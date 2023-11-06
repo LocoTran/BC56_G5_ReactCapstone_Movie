2937b7e59c82b4106345e394a20bc0b968148e04
TatzkDepression <tatzkrebecca@gmail.com>
2023-11-06 18:47:19 +0700

    them react-player va chinh cho khi click vao trailer se popup mini window

--- src/pages/HomePage/Hero.js
+++ src/pages/HomePage/Hero.js
@@ -2,53 +2,75 @@ import React, { useEffect, useState } from "react";
 import { movieService } from "../../services/service";
 import { Carousel } from "antd";
 import playBtn from "./play.png";
+import ReactPlayer from "react-player";
 
 export default function Hero() {
-    const [list, setList] = useState([]);
-    const contentStyle = {
-        height: "calc(75vh - 80px)",
-        width: "100%",
-        objectFit: "contain",
-        color: "#fff",
-        lineHeight: "160px",
-        textAlign: "center",
-        background: "rgb(254 215 170)",
-    };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTrailer, setSelectedTrailer] = useState("");
+  const [list, setList] = useState([]);
+  const contentStyle = {
+    height: "calc(75vh - 80px)",
+    width: "100%",
+    objectFit: "contain",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "rgb(254 215 170)",
+  };
 
-    useEffect(() => {
-        movieService
-            .getList()
-            .then((res) => {
-                setList(res.data.content);
-            })
-            .catch((err) => {
-                console.log("🚀👾👽 ~ err:", err);
-            });
-    }, []);
+  useEffect(() => {
+    movieService
+      .getList()
+      .then((res) => {
+        setList(res.data.content);
+      })
+      .catch((err) => {
+        console.log("🚀👾👽 ~ err:", err);
+      });
+  }, []);
 
-    let renderCarousel = () => {
-        return list.map(({ hinhAnh, maPhim, trailer }) => {
-            return (
-                <div key={maPhim} className="relative">
-                    <a href={trailer} target="_blank" rel="noopener">
-                        <img
-                            loading="lazy"
-                            src={playBtn}
-                            alt=""
-                            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-                        />
-                    </a>
-                    <img src={hinhAnh} alt="" style={contentStyle} />
-                </div>
-            );
-        });
-    };
-
-    return (
-        <div>
-            <Carousel autoplay className="active:border-none">
-                {renderCarousel()}
-            </Carousel>
+  let renderCarousel = () => {
+    return list.map(({ hinhAnh, maPhim, trailer }) => {
+      return (
+        <div key={maPhim} className="relative">
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener"
+            onClick={(e) => {
+              e.preventDefault();
+              setSelectedTrailer(trailer);
+              setIsModalOpen(true);
+            }}
+          >
+            <img
+              loading="lazy"
+              src={playBtn}
+              alt=""
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            />
+          </a>
+          <img src={hinhAnh} alt="" style={contentStyle} />
         </div>
-    );
+      );
+    });
+  };
+
+  return (
+    <div>
+      <Carousel autoplay>{renderCarousel()}</Carousel>
+      {isModalOpen && (
+        <div className="modal" onClick={() => setIsModalOpen(false)}>
+          <div className="modal-content">
+            <ReactPlayer
+              url={selectedTrailer}
+              width="100%" // Tăng kích thước chiều rộng của video
+              height="100%" // Tăng kích thước chiều cao của video
+              controls={true} // Hiển thị các nút điều khiển video
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
