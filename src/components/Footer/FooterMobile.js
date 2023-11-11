60c6bd6e6df03a55835e84a56b0bd10c875884a3
Loco Tran <phuloc2712@gmail.com>
2023-11-11 13:47:22 +0700

    react-movie

--- src/components/Footer/FooterMobile.js
+++ src/components/Footer/FooterMobile.js
@@ -13,35 +13,6 @@ export default function FooterMobile() {
                 <p className="">
                     Sản phẩm của nhóm 5 - BC56. <br />
                     Hoàn thành 11/2023. <br />
-                    Thành viên nhóm:
-                    <br />
-                    <a
-                        href="https://web.facebook.com/profile.php?id=100012335049289"
-                        rel="noopener"
-                        target="_blank"
-                        className="text-red-500 hover:text-red-700"
-                    >
-                        Trần Đại Phú Lộc
-                    </a>
-                    <br />
-                    <a
-                        href="https://web.facebook.com/nhan.truongthanh.19.03"
-                        rel="noopener"
-                        target="_blank"
-                        className="text-red-500 hover:text-red-700"
-                    >
-                        Trương Thành Nhân
-                    </a>
-                    <br />
-                    Link phân chia task:{" "}
-                    <a
-                        href="https://docs.google.com/spreadsheets/d/1138x6tlvRE4PRMpoK95BPKnzBOSNn5XuI-nEu3XdysY/edit#gid=0"
-                        rel="noopener"
-                        target="_blank"
-                        className="text-green-500 hover:text-green-700 mx-2"
-                    >
-                        GG sheet
-                    </a>
                 </p>
             </div>
         </div>
--- src/components/Footer/FooterTablet.js
+++ src/components/Footer/FooterTablet.js
@@ -13,33 +13,6 @@ export default function FooterTablet() {
                 <p className="text-right">
                     Sản phẩm của nhóm 5 - BC56. <br />
                     Hoàn thành 11/2023. <br />
-                    Thành viên nhóm:
-                    <a
-                        href="https://web.facebook.com/profile.php?id=100012335049289"
-                        rel="noopener"
-                        target="_blank"
-                        className="text-red-500 hover:text-red-700 ml-2"
-                    >
-                        Trần Đại Phú Lộc
-                    </a>
-                    <a
-                        href="https://web.facebook.com/nhan.truongthanh.19.03"
-                        rel="noopener"
-                        target="_blank"
-                        className="text-red-500 hover:text-red-700 ml-2"
-                    >
-                        Trương Thành Nhân
-                    </a>
-                    <br />
-                    Link phân chia task:{" "}
-                    <a
-                        href="https://docs.google.com/spreadsheets/d/1138x6tlvRE4PRMpoK95BPKnzBOSNn5XuI-nEu3XdysY/edit#gid=0"
-                        rel="noopener"
-                        target="_blank"
-                        className="text-green-500 hover:text-green-700 ml-2"
-                    >
-                        GG sheet
-                    </a>
                 </p>
             </div>
         </div>
