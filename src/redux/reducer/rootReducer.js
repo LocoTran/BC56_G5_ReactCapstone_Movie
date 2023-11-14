f0f0c917c13710d2466aa13651aa9f9f83397fc0
LocoTran <132773350+LocoTran@users.noreply.github.com>
2023-11-14 07:47:39 +0700

    Revert "Admin"

--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -1,9 +1,5 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./userReducer";
 import { spinnerReducer } from "./spinnerReducer";
-// import * as reducer from "./redux/reducer";
 
-export let rootReducer = combineReducers({
-  userReducer,
-  spinnerReducer,
-});
+export let rootReducer = combineReducers({ userReducer, spinnerReducer });
